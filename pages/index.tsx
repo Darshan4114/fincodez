9971a173314dfa8723c96d3a55bd11ec717cecd9
Darshan Dhabale <dhabaledarshan@dgmail.com>
2023-12-13 22:36:30 +0530

    Add. index page redirect to dashboard

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function MainPage() {
+    const router = useRouter();
+    useEffect(() => {
+        router.push('/dashboard');
+    }, [])
+}
